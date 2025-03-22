7a51ad83cb0abe83610bef340de55666c7fa03e1
nachigit27 <narasimha.athani@gmail.com>
2025-03-22 18:08:36 +0530

    changes from feature branch

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,12 +3,12 @@ package org.example;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome World");
     }
     public static void printSomething(){
         System.out.println("Print Something");
     }
     public static void doSomething(){
-        System.out.println("Do Something");
+        System.out.println("Please Do Something");
     }
 }
