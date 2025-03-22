c3888809971295c94c25b02489cbf20f97a852af
nachigit27 <narasimha.athani@gmail.com>
2025-03-22 16:54:44 +0530

    initial commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public static void printSomething(){
+        System.out.println("Print Something");
+    }
+}
