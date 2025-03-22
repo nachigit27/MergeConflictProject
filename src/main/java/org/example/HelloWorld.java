b76a8e46410e0974c09af4c55f3bf28daa1a23ff
nachigit27 <narasimha.athani@gmail.com>
2025-03-22 18:05:45 +0530

    modified helloworld method and printSomething method

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,10 +3,10 @@ package org.example;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World !!");
     }
     public static void printSomething(){
-        System.out.println("Print Something");
+        System.out.println("Please Print Something");
     }
     public static void doSomething(){
         System.out.println("Do Something");
